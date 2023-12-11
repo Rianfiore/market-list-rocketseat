50d35c9d7a990e96e3a2b69a9f61ca3f623047cc
Rian Fiore <rian.camara@eumedicoresidente.com.br>
2023-12-10 21:18:26 -0300

    ✨ feat(Checkbox): add Checkbox component

---
+++ src/components/atoms/Checkbox/Checkbox.test.tsx
@@ -0,0 +1,50 @@
+import { checkboxMock } from "@/__mocks__/checkbox";
+import "@testing-library/jest-dom";
+import { render } from "@testing-library/react";
+import { Checkbox } from ".";
+
+describe("Checkbox component", () => {
+  test("should render correctly", () => {
+    const { getByTestId } = render(<Checkbox />);
+    const checkbox = getByTestId("checkbox");
+
+    expect(checkbox).toBeInTheDocument();
+  });
+
+  test("should be unchecked by default", () => {
+    const { getByTestId } = render(<Checkbox />);
+    const checkbox = getByTestId("checkbox");
+
+    expect(checkbox).not.toBeChecked();
+  });
+
+  test("onChange event should be triggered when clicked", () => {
+    const { getByTestId } = render(
+      <Checkbox onChange={checkboxMock.onChange} />
+    );
+    const checkbox = getByTestId("checkbox");
+
+    checkbox.click();
+
+    expect(checkboxMock.onChange).toHaveBeenCalled();
+  });
+
+  test("should be checked when clicked", () => {
+    const { getByTestId } = render(<Checkbox />);
+    const checkbox = getByTestId("checkbox");
+
+    checkbox.click();
+
+    expect(checkbox).toBeChecked();
+  });
+
+  test("should be unchecked when clicked twice", () => {
+    const { getByTestId } = render(<Checkbox />);
+    const checkbox = getByTestId("checkbox");
+
+    checkbox.click();
+    checkbox.click();
+
+    expect(checkbox).not.toBeChecked();
+  });
+});
