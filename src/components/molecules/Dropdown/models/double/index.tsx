688d133e7d51ccf4f5205da9b201bf3cfc3cb3ae
Rian Fiore <rian.camara@eumedicoresidente.com.br>
2024-03-08 18:41:29 -0300

    🔧 fix(dropdown): add useRef hook to dropdown component to improve accessibility and focus management 🔧 fix(dropdown): add ref to the parent div element to improve accessibility and focus management

--- src/components/molecules/Dropdown/models/double/index.tsx
+++ src/components/molecules/Dropdown/models/double/index.tsx
@@ -12,7 +12,7 @@ export function DropdownDouble({
   const [isOpenedMenu, setIsOpenedMenu] = useState(false);
   const [selectValue, setSelectValue] = useState<string>(data[0].value!);
   const [isInputFocused, setIsInputFocused] = useState(false);
-
+  const dropdownRef = useRef(null);
   const inputRef = useRef<HTMLSpanElement>(null);
   const inputWidth = inputRef.current?.offsetWidth;
   const { ref: dropdownMenuRef } = useClickInOut({
@@ -23,53 +23,55 @@ export function DropdownDouble({
   });
 
   return (
-    <>
-      <div data-testid="dropdown-double" className="flex flex-col gap-2">
-        {label && (
-          <label
-            data-testid="dropdown-double-label"
-            data-isopenedmenu={isOpenedMenu}
-            data-isfocused={isInputFocused}
-            data-hasvalue={Boolean(selectValue)}
-            className="text-neutral-gray200 data-[isfocused=true]:text-brand-purple-light"
-          >
-            {label}
-          </label>
-        )}
-        <span className="flex">
-          <Input.Text
-            variant="RIGHT-STRAIGHT"
-            onFocus={() => setIsInputFocused(true)}
-            onBlur={() => setIsInputFocused(false)}
-            maxLength={8}
-            width={88}
-          />
+    <div
+      data-testid="dropdown-double"
+      className="flex flex-col gap-2"
+      ref={dropdownRef}
+    >
+      {label && (
+        <label
+          data-testid="dropdown-double-label"
+          data-isopenedmenu={isOpenedMenu}
+          data-isfocused={isInputFocused}
+          data-hasvalue={Boolean(selectValue)}
+          className="text-neutral-gray200 data-[isfocused=true]:text-brand-purple-light"
+        >
+          {label}
+        </label>
+      )}
+      <span className="flex">
+        <Input.Text
+          variant="RIGHT-STRAIGHT"
+          onFocus={() => setIsInputFocused(true)}
+          onBlur={() => setIsInputFocused(false)}
+          maxLength={8}
+          width={88}
+        />
 
-          <Input.Select
-            variant="LEFT-STRAIGHT"
-            isOpenedMenu={isOpenedMenu}
-            inputRef={inputRef}
-            handleCloseMenu={(newState) => {
-              setIsOpenedMenu(newState);
-            }}
-            width={72}
-            value={selectValue.toUpperCase()}
-          >
-            {isOpenedMenu && (
-              <DropdownMenu
-                dropdownMenuRef={dropdownMenuRef}
-                inputWidth={inputWidth}
-                data={data}
-                selectedValue={{ value: selectValue }}
-                onOptionChange={(option) => {
-                  setSelectValue(option.value!);
-                  onOptionChange?.(option);
-                }}
-              />
-            )}
-          </Input.Select>
-        </span>
-      </div>
-    </>
+        <Input.Select
+          variant="LEFT-STRAIGHT"
+          isOpenedMenu={isOpenedMenu}
+          inputRef={inputRef}
+          handleCloseMenu={(newState) => {
+            setIsOpenedMenu(newState);
+          }}
+          width={72}
+          value={selectValue.toUpperCase()}
+        >
+          {isOpenedMenu && (
+            <DropdownMenu
+              dropdownMenuRef={dropdownMenuRef}
+              inputWidth={inputWidth}
+              data={data}
+              selectedValue={{ value: selectValue }}
+              onOptionChange={(option) => {
+                setSelectValue(option.value!);
+                onOptionChange?.(option);
+              }}
+            />
+          )}
+        </Input.Select>
+      </span>
+    </div>
   );
 }
