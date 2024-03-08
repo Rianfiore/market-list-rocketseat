61fc0070150ca51fdf3241aff2ae636c928ad7db
Rian Fiore <rian.camara@eumedicoresidente.com.br>
2024-03-08 07:00:21 -0300

    ✨ feat(unitsMeasurement.ts): add unitsMeasurementEnum to provide a centralized and consistent way of representing units of measurement

---
+++ src/enum/unitsMeasurement.ts
@@ -0,0 +1,26 @@
+export const unitsMeasurementEnum = {
+  KG: {
+    value: "Kg",
+    symbol: "KG",
+    label: {
+      singular: "kg",
+      plural: "kg",
+    },
+  },
+  UN: {
+    value: "Un.",
+    symbol: "UN.",
+    label: {
+      singular: "unidade",
+      plural: "unidades",
+    },
+  },
+  L: {
+    value: "L",
+    symbol: "L",
+    label: {
+      singular: "litro",
+      plural: "litros",
+    },
+  }
+};
